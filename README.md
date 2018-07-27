0d504c55acef0dc37fecf0ca8072748a7a312489
Andrey Galkin <andrey@futoin.org>
2018-07-27 13:34:47 +0300

    Prepared base for AsyncSteps RI

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### FutoIn AsyncSteps reference C++ implementation
+
+See [**FTN12: AsyncSteps**](https://futoin.org/docs/asyncsteps/) for more details.
---
+++ include/futoin/ri/asyncsteps.hpp
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------------
+//   Copyright 2018 FutoIn Project
+//   Copyright 2018 Andrey Galkin
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//-----------------------------------------------------------------------------
+//! @file
+//! @brief Reference Implementation of AsyncSteps (FTN8)e for C++
+//! @sa https://specs.futoin.org/final/preview/ftn12_async_api.html
+//-----------------------------------------------------------------------------
+
+#ifndef FUTOIN_RI_ASYNCSTEPS_HPP
+#define FUTOIN_RI_ASYNCSTEPS_HPP
+//---
+#include <futoin/asyncsteps.hpp>
+//---
+
+namespace futoin {
+    namespace ri {
+        class AsyncSteps : public futoin::AsyncSteps
+        {};
+    } // namespace ri
+} // namespace futoin
+
+//---
+#endif // FUTOIN_RI_ASYNCSTEPS_HPP
