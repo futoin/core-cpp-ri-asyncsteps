d5b8ff7710e7b3590e973e046a546d14095fbc65
Andrey Galkin <andrey@futoin.org>
2023-05-15 13:39:03 +0300

    CHANGED: dependency maintenance

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,7 @@
+
+# See: https://github.com/boostorg/fiber/issues/301
+# Still a build error...
+# hunter_config(Boost
+#    VERSION ${HUNTER_Boost_VERSION}
+#    CMAKE_ARGS
+#    CONFIG_MACRO=BOOST_FIBERS_NO_ATOMICS)
